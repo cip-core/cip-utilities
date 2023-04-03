2e0773295469ff9bf0bf250a6fb60e9ec73ff46b
patricelachance <patrice.lachance@socgen.com>
2023-04-03 09:18:45 +0000

    adding addtional vault functions

--- cip-helpers-functions.sh
+++ cip-helpers-functions.sh
@@ -23,3 +23,41 @@ function vault_load_key_in_env() {
         for key in $(echo $JSON_OBJ | jq -r 'keys[]'); do export "$key=$(echo $JSON_OBJ | jq -r ".$key")"; done
     fi
 }
+
+function vault_get_key() {
+    if [ -z "$1" ]; then
+        _err "Please provide vault key name"
+    else
+        vaultKey=$1
+
+        # Pulling data from the Vault
+        JSON_OBJ=$(vault kv get -format json $VAULT_MOUNT/$VAULT_TOP_DIR/$vaultKey | jq '.data.data | del(.[".onyxia"])')
+
+        # Exporting keys and corresponding values as environment variables
+        for key in $(echo $JSON_OBJ | jq -r 'keys[]'); do echo "$key=$(echo $JSON_OBJ | jq -r ".$key")"; done
+    fi
+}
+
+function vault_patch_files() {
+    if [ -z "$1" ]; then
+        _err "Please provide vault key name"
+    else
+        vaultKey=$1
+
+        # Pulling data from the Vault
+        JSON_OBJ=$(vault kv get -format json $VAULT_MOUNT/$VAULT_TOP_DIR/$vaultKey | jq '.data.data | del(.[".onyxia"])')
+
+        # Exporting keys and corresponding values as environment variables
+        (
+          for key in $(echo $JSON_OBJ | jq -r 'keys[]'); do 
+            export "$key=$(echo $JSON_OBJ | jq -r ".$key")"
+          done
+          VARIABLES_TO_FILL=$(env | awk -F = '{printf " ${%s}", $1}')
+          for file in $(find ./ -type f); do 
+            mv $file $file.bak 
+            envsubst "$VARIABLES_TO_FILL" < $file.bak > $file
+          done
+        )
+    fi
+}
+
